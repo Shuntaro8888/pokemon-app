9ca47d7f31f0e67660a86b375319329889c5c14b
Ryotaro <ryotaro20250303@gmail.com>
2025-10-04 21:07:30 +0900

    Initial commit

---
+++ src/utils/pokemon.js
@@ -0,0 +1,19 @@
+//  データのロードには少々時間がかかることを想定
+export const getAllPokemon = (url) => {
+  return new Promise((resolve, reject) => {
+    fetch(url)
+      .then((res) => res.json()) //Json形式にして
+      .then((data) => resolve(data)); //値を返す
+  });
+};
+
+export const getPokemon = (url) => {
+  return new Promise((resolve, reject) => {
+    fetch(url)
+      .then((res) => res.json()) //Json形式にして
+      .then((data) => {
+        // console.log(data);
+        resolve(data);
+      }); //値を返す
+  });
+};
